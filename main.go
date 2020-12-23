41d74d70b2e473e51358046de878bcc449bd6d34
Patricia Tozi <patricia.tozi@votorantim.com>
2020-12-22 21:38:35 -0300

    Creating the main file

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+    "fmt"
+    "log"
+    "os"
+)
+
+func main() {
+
+	// Step 1. Read txt file line by line
+
+	// Step 2. Data cleaning and treatment
+
+	// Step 3. Importing data in DB
+}
